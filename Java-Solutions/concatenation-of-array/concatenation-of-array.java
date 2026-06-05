f3ba9566667e1c180e1643efb8a568333a9c5001
Bindu2419 <binduchinta0@gmail.com>
2026-06-05 16:35:49 +0530

    Delete Java-Solutions directory

--- Java-Solutions/concatenation-of-array/concatenation-of-array.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public int[] getConcatenation(int[] nums) {
-        int n = nums.length;
-        int[] result = Arrays.copyOf(nums, 2 * n);  // copy nums into size 2n
-        System.arraycopy(nums,0,result,n,n);
-        return result;
-    }
-}
