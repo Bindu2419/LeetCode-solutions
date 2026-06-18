feee31f7165cf9fe37da804902489c191bdbd0d5
Bindu2419 <binduchinta0@gmail.com>
2026-06-18 11:19:40 +0530

    Time: 600 ms (5.03%), Space: 47 MB (12.85%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,59 +1,95 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
 
-        Arrays.sort(nums);
+        Set<List<Integer>> ans = new HashSet<>();
 
-        List<List<Integer>> ans = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++) {
 
-        for(int i = 0; i < nums.length - 3; i++) {
+            for(int j = i + 1; j < nums.length; j++) {
 
-            if(i > 0 && nums[i] == nums[i - 1])
-                continue;
+                HashSet<Long> set = new HashSet<>();
 
-            for(int j = i + 1; j < nums.length - 2; j++) {
+                for(int k = j + 1; k < nums.length; k++) {
 
-                if(j > i + 1 && nums[j] == nums[j - 1])
-                    continue;
+                    long fourth = (long)target - nums[i] - nums[j] - nums[k];
 
-                int left = j + 1;
-                int right = nums.length - 1;
+                    if(set.contains(fourth)) {
 
-                while(left < right) {
-
-                    long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if(sum == target) {
-
-                        ans.add(Arrays.asList(
+                        List<Integer> temp = Arrays.asList(
                             nums[i],
                             nums[j],
-                            nums[left],
-                            nums[right]
-                        ));
+                            nums[k],
+                            (int)fourth
+                        );
 
-                        while(left < right && nums[left] == nums[left + 1])
-                            left++;
-
-                        while(left < right && nums[right] == nums[right - 1])
-                            right--;
-
-                        left++;
-                        right--;
-
+                        Collections.sort(temp);
+                        ans.add(temp);
                     }
-                    else if(sum < target) {
-                        left++;
-                    }
-                    else {
-                        right--;
-                    }
+
+                    set.add((long)nums[k]);
                 }
             }
         }
 
-        return ans;
+        return new ArrayList<>(ans);
     }
 }
+// class Solution {
+//     public List<List<Integer>> fourSum(int[] nums, int target) {
+
+//         Arrays.sort(nums);
+
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         for(int i = 0; i < nums.length - 3; i++) {
+
+//             if(i > 0 && nums[i] == nums[i - 1])
+//                 continue;
+
+//             for(int j = i + 1; j < nums.length - 2; j++) {
+
+//                 if(j > i + 1 && nums[j] == nums[j - 1])
+//                     continue;
+
+//                 int left = j + 1;
+//                 int right = nums.length - 1;
+
+//                 while(left < right) {
+
+//                     long sum = (long)nums[i] + nums[j] + nums[left] + nums[right];
+
+//                     if(sum == target) {
+
+//                         ans.add(Arrays.asList(
+//                             nums[i],
+//                             nums[j],
+//                             nums[left],
+//                             nums[right]
+//                         ));
+
+//                         while(left < right && nums[left] == nums[left + 1])
+//                             left++;
+
+//                         while(left < right && nums[right] == nums[right - 1])
+//                             right--;
+
+//                         left++;
+//                         right--;
+
+//                     }
+//                     else if(sum < target) {
+//                         left++;
+//                     }
+//                     else {
+//                         right--;
+//                     }
+//                 }
+//             }
+//         }
+
+//         return ans;
+//     }
+// }
 // class Solution {
 //     public List<List<Integer>> fourSum(int[] nums, int target) {
 //         Set<List<Integer>> set=new HashSet<>();
